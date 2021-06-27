eca117a5768a79d220119217aee9c672238218c8
3Gigs <yanghuaxuan@gmail.com>
2021-06-27 16:26:03 -0700

    screw curses

--- __main__.py
+++ __main__.py
@@ -11,6 +11,8 @@ def initGame(stdscr):
         else:
             stdscr.addstr("O")
     stdscr.addstr("Welcome to tic tac toe with Curses!\n")
+    stdscr.addstr(str(LINES) + "\n")
+    stdscr.addstr(str(COLS) + "\n")
     stdscr.addstr("Created by Huaxuan Yang (3Gigs)\n")
     stdscr.addstr("Please press ENTER to start game!\n", curses.A_BOLD)
     stdscr.addstr("Press Q to exit game\n", curses.A_BOLD)
@@ -18,7 +20,9 @@ def initGame(stdscr):
     while True:
         key = stdscr.getch()
         if key == ord('\n' or '\r'):
-            newGame(LINES, COLS)
+            stdscr.clear()
+            stdscr.refresh()
+            newGame()
         if key == ord('q'):
             break
                 
--- tictacgame.py
+++ tictacgame.py
@@ -1,24 +1,36 @@
 import curses
 import math
 
+# TODO, this breaks
 def drawBoard(gameWin, BOARDCOLS, BOARDLINES):
     for i in range(BOARDCOLS):
-        gameWin.addstr(math.floor(BOARDLINES / 3), i, "-")
-        gameWin.addstr(math.floor(BOARDLINES / 1.5), i, "-")
-        
-    for i in range(BOARDLINES):
-        gameWin.addstr(i, math.floor(BOARDCOLS / 3), "|")
-        gameWin.addstr(i, math.floor(BOARDCOLS / 1.5), "|")
-    
-
-
-
-def newGame(LINES, COLS):
-    gameWin = curses.newwin(LINES, COLS, 0, 0)
-    BOARDCOLS = math.floor(COLS / 2)
-    BOARDLINES = LINES - 4
-    
-    drawBoard(gameWin, BOARDCOLS, BOARDLINES)
-    
-    gameWin.addstr(BOARDLINES, 0, "XD")
+        gameWin.addstr(math.floor(BOARDCOLS / 8), i,'-')
+        gameWin.addstr(math.floor(BOARDCOLS / 4), i, '-')
+    for i in range(BOARDLINES - 2):
+        gameWin.addstr(i, math.floor(BOARDCOLS / 3), '|')
+        gameWin.addstr(i, math.floor(BOARDCOLS / 1.5), '|')
     gameWin.refresh()
+        
+
+
+def newGame():
+    BOARD_COLS = 24;
+    BOARD_LINES = 12;
+
+    gameWin = curses.newwin(BOARD_LINES, BOARD_COLS, 0, 0)
+    gameWin.keypad(True)
+    drawBoard(gameWin, BOARD_COLS, BOARD_LINES)
+    gameWin.move(0, 0)
+    while True:
+        y, x = gameWin.getyx()
+        key = gameWin.get_wch()
+        if(key == curses.KEY_UP):
+            gameWin.move(y - 1, 0)
+        elif (key == curses.KEY_DOWN):
+            gameWin.move(y + 1, 0)
+        elif (key == curses.KEY_RIGHT):
+            gameWin.move(0, 1)
+        elif (key == curses.KEY_LEFT):
+            gameWin.move(0, -1)
+        else:
+            pass
