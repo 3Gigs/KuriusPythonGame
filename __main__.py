405b80e305587f1641c0b9661a7909d37c0be4aa
3Gigs <yanghuaxuan@gmail.com>
2021-06-27 00:10:46 -0700

    Using curses instead of qt

--- __main__.py
+++ __main__.py
@@ -1,11 +1,2 @@
-from pygame import MyWidget
-from PySide6 import QtCore, QtWidgets, QtGui
-import sys
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
-    
-    sys.exit(app.exec())
+if __name__ == '__main__':
+    print('cyka')
