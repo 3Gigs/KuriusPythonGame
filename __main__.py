9fea4d9eed75c9381490a7addad3df9baa9ff4a0
3Gigs <yanghuaxuan@gmail.com>
2021-06-27 01:18:44 -0700

    Title screen fully works

--- __main__.py
+++ __main__.py
@@ -1,2 +1,29 @@
+import curses
+
+def newGame(stdscr):
+    print("xd")
+
+def initGame(stdscr):
+    ROWS, COLS = stdscr.getmaxyx()
+
+    stdscr.clear()
+    for i in range(COLS):
+        if(i % 2):
+            stdscr.addstr("X")
+        else:
+            stdscr.addstr("O")
+    stdscr.addstr("Welcome to tic tac toe with Curses!\n")
+    stdscr.addstr("Created by Huaxuan Yang (3Gigs)\n")
+    stdscr.addstr("Please press ENTER to start game!\n", curses.A_BOLD)
+    stdscr.addstr("Press Q to exit game\n", curses.A_BOLD)
+    
+    while True:
+        key = stdscr.getch()
+        if key == ord('\n'):
+            stdscr.addstr("xd\n")
+            stdscr.refresh()
+        if key == ord('q'):
+            stdscr.endwin()
+                
 if __name__ == '__main__':
-    print('cyka')
+    curses.wrapper(initGame)
